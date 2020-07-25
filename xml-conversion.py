19934f09c4a11ac90016e22cc14bdc10c7447964
Eric <emtilmont@gmail.com>
2020-07-25 11:02:29 -0400

    finish up script

--- xml-conversion.py
+++ xml-conversion.py
@@ -2,6 +2,7 @@ import os
 from xml.dom.minidom import parse
 
 path = ''
+logfile = os.path.join(os.getcwd(), 'output.txt')
 with open('paths.txt', 'r') as file:
     path = file.read().replace('\n', '').strip()
 
@@ -9,10 +10,12 @@ filepaths = []
 for dirpath, dirnames, filenames in os.walk(path):
     for f in filenames:
         filepaths.append(os.path.join(dirpath, f))
-        
 
 for filepath in filepaths:
-    dom = parse(filepath)
-    locs = dom.getElementsByTagName('loc')
-    for loc in locs:
-        print(loc)
+    if 'log.txt' not in filepath:
+        fxml = open(filepath)
+        dom = parse(fxml)
+        locs = dom.getElementsByTagName('loc')
+        for loc in locs:
+            with open(logfile, 'a') as f:
+                f.write(loc + '\n')
